42e676740ba0bc7da437f4f4716bbd0f67c9fe02
Gustavo Pironi <gupironi@hotmail.com>
2024-10-15 14:14:41 -0300

    Estudos dia 15/10
    Estudos a respeito de lista, laços e exceções

---
+++ python-poo/exercicios/ex02.py
@@ -0,0 +1,32 @@
+numero = [1,2,3,4,5,6,7,8,9,10]
+nomes = ['Carlos', 'Gustavo', 'Emirhan', 'Erick']
+anos = [2003, 2024]
+
+lista = [1,23,2,45,25,99]
+for i in lista:
+    print(i)
+
+soma_impares = 0
+for i in range(1,11,2):
+    soma_impares += i
+print(soma_impares)
+
+for a in range(10,0,-1):
+    print(a)
+
+#number = int(input('Digite o numero'))
+#for i in range (1,11,1):
+#    print(number * i)
+
+list = [22,36,15,49]
+soma = 0
+
+try:
+    for i in list:
+        soma += i
+    media = soma / len(list)
+    print(f'Media dos valores: {media}')
+except ZeroDivisionError:
+    print('A lista está vazia')
+except Exception as e:
+    print(f'Ocorreu um erro: {e}')
--- python-poo/sabor-express/app.py
+++ python-poo/sabor-express/app.py
@@ -1,5 +1,7 @@
 import os
 
+restaurantes = ['Pizza', 'Sushi']
+
 def exibir_nome_programa():
     print("""
     ╭━━━╮╱╱╭╮╱╱╱╱╱╱╱╭━━━╮
@@ -18,24 +20,57 @@ def exibir_opcoes():
     print('3. Ativar restaurante')
     print('4. Sair\n')
 
+def voltar_menu():
+    input('\nDigite uma tecla para voltar ao menu principal\n')
+    main()
+
+def exibir_subtitulo(texto):
+    os.system('cls')
+    print(texto)
+    print()
+
+def cadastrar_restaurante():
+    exibir_subtitulo('Cadastro de novos restaurantes')
+    nome_do_restaurante = input('Digite o nome do restaurante que deseja cadastrar: ')
+    restaurantes.append(nome_do_restaurante)
+    print(f'O restaurante *{nome_do_restaurante}* foi cadastrado com sucesso!\n')
+    voltar_menu()
+
+def listar_restaurantes():
+    exibir_subtitulo('Listando os restaurantes')
+
+    for restaurante in restaurantes:
+        print(f'.{restaurante}')
+    
+    voltar_menu()
+
 def escolher_opcao():
-    opcao_escolhida = int(input('Escolha uma opção: '))
-    print(f'Você escolheu a opção {opcao_escolhida}')
+    try:
+        opcao_escolhida = int(input('Escolha uma opção: '))
+        print(f'Você escolheu a opção {opcao_escolhida}')
+        
+        if opcao_escolhida == 1:
+            cadastrar_restaurante()
+        elif opcao_escolhida == 2:
+            listar_restaurantes()
+        elif opcao_escolhida == 3:
+            print('Ativar restaurante')
+        elif opcao_escolhida == 4:
+            finalizar_app()
+        else:
+            opcao_invalida()
+    except:
+        opcao_invalida()
 
-    def finalizar_app():
-        os.system('clear')
-        print('Encerrando o programa')
+def opcao_invalida():
+    print('Opção inválida!\n')
+    voltar_menu()
 
-    if opcao_escolhida == 1:
-        print('Cadastrar restaurante')
-    elif opcao_escolhida == 2:
-        print('Listar restaurantes')
-    elif opcao_escolhida == 3:
-        print('Ativar restaurante')
-    else:
-        finalizar_app()
+def finalizar_app():
+    exibir_subtitulo('Encerrando o programa')
 
 def main():
+    os.system('cls')
     exibir_nome_programa()
     exibir_opcoes()
     escolher_opcao()
