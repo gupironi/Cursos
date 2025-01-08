664a61d1f8660e839c6f2d5e7ca80a4a75e0de7b
Gustavo Pironi <gustavo.pironi@nubank.com.br>
2025-01-08 08:10:53 -0300

    Estudos dia 08/01

--- python-poo/oo-sabor-express/app.py
+++ python-poo/oo-sabor-express/app.py
@@ -1,12 +1,14 @@
 from modelos.restaurante import Restaurante
+from modelos.cardapio.bebida import Bebida
+from modelos.cardapio.prato import Prato
 
 restaurante_praca = Restaurante('praça', 'Gourmet')
-restaurante_praca.receber_avaliacao('Gugu', 5)
-restaurante_praca.receber_avaliacao('Gaby', 3.7)
-restaurante_praca.receber_avaliacao('AAAAA', 4)
+bebida_suco = Bebida('Monster', 6.9, 'Gigrande')
+prato_teste = Prato('Burgao', 27.8, 'O burgao paulistao')
 
 def main():
-    Restaurante.listar_restaurantes()
+    print(bebida_suco)
+    print(prato_teste)
 
 if __name__ == '__main__':
     main()
